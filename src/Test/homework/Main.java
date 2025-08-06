db57a1884490cfd7749fccd73ba53c5a26c771fb
kim se hyuen <ksjk711@gmail.com>
2025-08-06 15:49:30 +0900

    first commit

--- src/Test/homework/Main.java
+++ src/Test/homework/Main.java
@@ -1,4 +1,15 @@
 package Test.homework;
 
 public class Main {
+
+    public void stringMaker(){
+
+    }
+    public static void main(String[] args) {
+
+
+        String str1 = new String("abcd");
+        str1.length()
+        System.out.println(str1.charAt(0));
+    }
 }
