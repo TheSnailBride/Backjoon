5fc4c22e0938e17672c1c8eb22dd581a02758590
kim se hyuen <ksjk711@gmail.com>
2025-08-27 13:19:00 +0900

    first commit

--- src/Test/homework/work0801_2/Car.java
+++ src/Test/homework/work0801_2/Car.java
@@ -1,13 +1,13 @@
 package Test.homework.work0801_2;
 
-public class Car extends Vehicle{
+public class Car extends Vehicle {
 
     Car(String brand, int year) {
         super(brand, year);
     }
 
     @Override
-    public void printInfo(){
+    public void printInfo() {
         System.out.println("Car 이 움직입니다.");
     }
 
