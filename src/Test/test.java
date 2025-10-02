124c2aed913a59136fccc19eda2be6c2c7dddaee
kim se hyuen <ksjk711@gmail.com>
2025-10-02 13:24:23 +0900

    1002

--- src/Test/test.java
+++ src/Test/test.java
@@ -1,23 +1,22 @@
 package Test;
 
-import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
 
 
-        int[] array1 = new int[3];
 
-        array1 = new int[]{1, 2, 3};
-
-//        int[][] array = new int[2][3];
-//
-//        array[0] = {1,2,3};
+        int m = 10 ;
+        int a = 10;
 
 
-        System.out.println(array1[0]);
+        fun();
+        System.out.println(a);
 
+    }
 
+    public static void fun(){
+        int a = 20;
 
-}
+    }
 }
