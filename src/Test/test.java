f24abe418b9ce19154aa62fdd6814e3e10084658
kim se hyuen <ksjk711@gmail.com>
2025-10-13 10:15:59 +0900

    1013

--- src/Test/test.java
+++ src/Test/test.java
@@ -1,22 +1,24 @@
 package Test;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
 public class test {
     public static void main(String[] args) {
 
 
 
-        int m = 10 ;
-        int a = 10;
+        ArrayList<Integer> list = new ArrayList<>();
 
+        list.add(1);
+        list.add(2);
+        list.add(3);
 
-        fun();
-        System.out.println(a);
-
-    }
-
-    public static void fun(){
-        int a = 20;
-
+        System.out.println(list.size());
+        System.out.println(list.isEmpty());
+        System.out.println(list.get(0));
+        Collections.sort(list);
     }
 }
