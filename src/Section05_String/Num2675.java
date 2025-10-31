0bf386283d4549a8d2af7549990a989b67872200
kim se hyuen <ksjk711@gmail.com>
2025-10-31 13:03:44 +0900

    1031

---
+++ src/Section05_String/Num2675.java
@@ -0,0 +1,21 @@
+package Section05_String;
+
+import java.util.Scanner;
+
+public class Num2675 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        int n = in.nextInt();
+
+        for(int i=0; i<n; i++){
+            int a = in.nextInt();
+            String str = in.nextLine();
+
+
+        }
+
+
+    }
+}
---
+++ src/Section05_String/Num27866.java
@@ -0,0 +1,16 @@
+package Section05_String;
+
+import java.util.Scanner;
+
+public class Num27866 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        String str = in.nextLine();
+
+        int n = in.nextInt();
+
+        System.out.println(str.charAt(n-1));
+    }
+}
