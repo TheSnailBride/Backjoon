37b9e76fbd5901b7afaab50767acbced42eed9cd
kim se hyuen <ksjk711@gmail.com>
2025-12-11 12:57:05 +0900

    1211

--- src/Section12_BruteForce/Num2839.java
+++ src/Section12_BruteForce/Num2839.java
@@ -2,29 +2,25 @@ package Section12_BruteForce;
 
 import java.util.Scanner;
 
-public class Num2839 {
+public class Num2839{
     public static void main(String[] args) {
 
         Scanner in = new Scanner(System.in);
 
-        int n = in.nextInt();
-        int hap;
+        int N = in.nextInt();
 
-            //n을 5로 나눈 몫 , n의 나머지 , hap 에는 몫을 넣는다
 
-        hap = n / 5 ; //n을 5로 나눈 몫
-        n = n % 5; // 나머지 개수 초기화
-
-        if(n>=3){
-            hap++;
-            n=n-3;
-        }
-        if(n!=0) {
+        if (N == 4 || N == 7) {
             System.out.println(-1);
-        }else{
-            System.out.println(hap);
         }
-
-
+        else if (N % 5 == 0) {
+            System.out.println(N / 5);
+        }
+        else if (N % 5 == 1 || N % 5 == 3) {
+            System.out.println((N / 5) + 1);
+        }
+        else if (N % 5 == 2 || N % 5 == 4) {
+            System.out.println((N / 5) + 2);
+        }
     }
 }
