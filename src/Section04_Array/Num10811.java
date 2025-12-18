41be60cb6dc7a588fe4f2d887528b061b69e2a34
kim se hyuen <ksjk711@gmail.com>
2025-12-18 11:50:18 +0900

    1218

---
+++ src/Section04_Array/Num10811.java
@@ -0,0 +1,37 @@
+package Section04_Array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Num10811 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int [] arr = new int[N];
+        for(int i=0; i<arr.length; i++){
+            arr[i] = i+1;
+
+        }
+
+        for(int i=0; i<M; i++){
+            int a = sc.nextInt()-1;
+            int b = sc.nextInt()-1;
+
+            for(int j= a; j<b; j++){
+
+                while(a<b) {
+                    int temp = arr[a];
+                    arr[a] = arr[b];
+                    arr[b] = temp;
+                    a++;
+                    b--;
+                }
+            }
+
+        }
+
+        for(int a: arr) System.out.print(a +" ");
+    }
+}
